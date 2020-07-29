cc54412dd0ee86a29352f8eb89db5fc980d714c6
youngjay90 <3254048915@qq.com>
2020-07-29 18:50:37 +0200

    refactor: Move data access classes to DataAccess folder and rename namespaces accordingly

---
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess
+{
+    public class TextConnector : IDataConnection
+    {
+        // TODO - wire up the CreatePrize for text files.
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            model.Id = 1;
+            return model;
+        }
+    }
+}
