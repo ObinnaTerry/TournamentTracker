f17e49cfe158f116807d1b25cd0fa0c6c95efdd1
youngjay90 <3254048915@qq.com>
2020-07-30 13:58:46 +0200

    refactor: Make changes that allows for a single database connection at a time

--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using TrackerLibrary.DataAccess;
 
@@ -7,21 +9,33 @@ namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
-        public static void InitializeConnections(bool database, bool textFiles)
+        public static IDataConnection Connection { get; private set; }
+
+        public static void InitializeConnections(DatabaseType db)
         {
-            if (database)
+            if (db == DatabaseType.Sql)
             {
                 // TODO - Create the SQL Connection properly
                 SqlConnector sql = new SqlConnector();
-                Connections.Add(sql);
+                Connection = sql;
             }
-            if (textFiles)
+            else if (db == DatabaseType.TextFile)
             {
                 // TODO - Create the Text Connection
                 TextConnector text = new TextConnector();
-                Connections.Add(text);
+                Connection = text;
             }
+        }
+
+        public static string CnnString()
+        {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            string strConnection = builder.Build().GetConnectionString("Tournament");
+
+            return strConnection;
         }
     }
 }
