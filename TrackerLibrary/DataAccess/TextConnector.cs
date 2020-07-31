cd7e1c1863a3358df1d40605ec7b75b269f4977d
youngjay90 <3254048915@qq.com>
2020-07-31 12:52:26 +0200

    refactor: Add code for saving to file.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
+using System.Linq;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
         // TODO - wire up the CreatePrize for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+
+
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            model.Id = currentId;
+
+            prizes.Add(model);
+
+            prizes.SaveToPrizeFile(PrizesFile);
+
             return model;
         }
     }
