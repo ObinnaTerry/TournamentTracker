a5936a9b601c496c2a5df574719ee45b514a29b5
youngjay90 <3254048915@qq.com>
2020-08-06 15:02:43 +0200

    feat: Add classes to handle saving person and team models to the database

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Data;
+using System.Linq;
 using System.Text;
 using TrackerLibrary.Models;
 
@@ -11,6 +12,30 @@ namespace TrackerLibrary.DataAccess
 {
     public class SqlConnector : IDataConnection
     {
+        /// <summary>
+        /// Takes person model and saves it to SQL database.
+        /// </summary>
+        /// <param name="model">Person model</param>
+        /// <returns>Person model</returns>
+        public PersonModel CreatePerson(PersonModel model)
+        {
+            using (SqlConnection connection = new SqlConnection(GlobalConfig.CnnString()))
+            {
+                var p = new DynamicParameters();
+                p.Add("@FirstName", model.FirstName);
+                p.Add("@LastName", model.LastName);
+                p.Add("@EmailAddress", model.EmailAddress);
+                p.Add("@CellphoneNumber", model.CellphoneNumber);
+                p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("dbo.spPeople_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+                return model;
+
+            }
+        }
+
         /// <summary>
         /// Saves a new prize to the database
         /// </summary>
@@ -30,10 +55,58 @@ namespace TrackerLibrary.DataAccess
                 connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
 
                 model.Id = p.Get<int>("@id");
+
+                return model;
+            }
+            
+        }
+
+        /// <summary>
+        /// Takes a team model and saves it to an SQL database. 
+        /// </summary>
+        /// <param name="model">Team model</param>
+        /// <returns>model</returns>
+        public TeamModel CreateTeam(TeamModel model)
+        {
+            using (SqlConnection connection = new SqlConnection(GlobalConfig.CnnString()))
+            {
+                var p = new DynamicParameters();
+                p.Add("@TeamName", model.TeamName);
+                
+                p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("dbo.spTeams_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+
+                foreach (PersonModel tm in model.TeamMembers)
+                {
+                    p = new DynamicParameters();
+                    p.Add("@TeamId", model.Id);
+                    p.Add("@PersonId", tm.Id);
+
+                    connection.Execute("dbo.spTeamMembers_Insert", p, commandType: CommandType.StoredProcedure);
+                }
+
                 return model;
 
             }
-            
+        }
+
+        /// <summary>
+        /// Queries the dtabase and fetched all records from the person table
+        /// </summary>
+        /// <returns>List of person model</returns>
+        public List<PersonModel> GetPerson_All()
+        {
+            List<PersonModel> output;
+
+            using(SqlConnection connection = new SqlConnection(GlobalConfig.CnnString()))
+            {
+                output = connection.Query<PersonModel>("dbo.spPeople_GetAll").ToList();
+            }
+
+            return output;
         }
     }
 }
