7aeaa8892ee590606141d43bf71648a2f2b31ab1
youngjay90 <3254048915@qq.com>
2020-08-06 15:05:10 +0200

    feat: Add helper functions for file storage loading and saving of person and team models

--- TrackerLibrary/DataAccess/TextConnectorProcessor.cs
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using TrackerLibrary.Models;
 
@@ -35,7 +36,7 @@ namespace TrackerLibrary.DataAccess.TextHelpers
         }
 
         /// <summary>
-        /// Converts loaded string list to a list of models. 
+        /// Converts loaded string list to a list of prize models. 
         /// </summary>
         /// <param name="lines">List of string</param>
         /// <returns>List of models.</returns>
@@ -62,9 +63,67 @@ namespace TrackerLibrary.DataAccess.TextHelpers
         }
 
         /// <summary>
-        /// Converts list of modles to a list of strings and saves the result to a file.
+        /// Converts loaded string list to a list of person models.
         /// </summary>
-        /// <param name="models">List of models to be converted to list of strings.</param>
+        /// <param name="lines">List of string</param>
+        /// <returns>List of models.</returns>
+        public static List<PersonModel> ConvertToPersonModels(this List<string> lines)
+        {
+            List<PersonModel> output = new List<PersonModel>();
+
+            foreach (string line in lines)
+            {
+                string[] cols = line.Split(',');
+
+                PersonModel p = new PersonModel();
+                p.Id = int.Parse(cols[0]);
+                p.FirstName = cols[1];
+                p.LastName = cols[2];
+                p.EmailAddress = cols[3];
+                p.CellphoneNumber = cols[4];
+                output.Add(p);
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Loads a Team data storage file and convert the content to team models.
+        /// </summary>
+        /// <param name="lines">List of each line of data in the Team storage file.</param>
+        /// <param name="peopleFileName">Name of storage fiel for People data.</param>
+        /// <returns></returns>
+        public static List<TeamModel> ConvertToTeamModels(this List<string> lines, string peopleFileName)
+        {
+            List<TeamModel> output = new List<TeamModel>();
+            List<PersonModel> people = peopleFileName.FullFilePath().LoadFile().ConvertToPersonModels();
+
+
+            foreach (string line in lines)
+            {
+                string[] cols = line.Split(',');
+
+                TeamModel t = new TeamModel();
+                t.Id = int.Parse(cols[0]);
+                t.TeamName = cols[1];
+
+                string[] personIds = cols[2].Split('|');
+
+                foreach (string id in personIds)
+                {
+                    t.TeamMembers.Add(people.Where(x => x.Id == int.Parse(id)).First());
+                }
+
+                output.Add(t);
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Converts list of prize models to a list of strings and saves the result to a file.
+        /// </summary>
+        /// <param name="models">List of prize models to be converted to list of strings.</param>
         /// <param name="fileName">Target file name</param>
         public static void SaveToPrizeFile(this List<PrizeModel> models, string fileName)
         {
@@ -72,10 +131,68 @@ namespace TrackerLibrary.DataAccess.TextHelpers
 
             foreach (PrizeModel p in models)
             {
-                lines.Add($"{ p.Id }, { p.PlaceNumber }, { p.PlaceName }, { p.PrizeAmount }, { p.PrizePercentage }");
+                lines.Add($"{ p.Id },{ p.PlaceNumber },{ p.PlaceName },{ p.PrizeAmount },{ p.PrizePercentage }");
             }
 
             File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+
+        /// <summary>
+        ///  Converts list of person models to a list of strings and saves the result to a file.
+        /// </summary>
+        /// <param name="models">List of person models to be converted to list of strings.</param>
+        /// <param name="fileName">Target file name</param>
+        public static void SaveToPeopleFile(this List<PersonModel> models, string fileName)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (PersonModel p in models)
+            {
+                lines.Add($"{ p.Id },{ p.FirstName },{ p.LastName },{ p.EmailAddress },{ p.CellphoneNumber }");
+            }
+
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+
+        /// <summary>
+        /// Converts list of team models to a list of strings and saves the result to a file.
+        /// </summary>
+        /// <param name="models">List of team models</param>
+        /// <param name="fileName">storage file name</param>
+        public static void SaveToTeamFile(this List<TeamModel> models, string fileName)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (TeamModel t in models)
+            {
+                lines.Add($"{ t.Id },{ t.TeamName },{ ConvertPeopleListToString(t.TeamMembers) }");
+            }
+
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+
+        /// <summary>
+        /// Iterates through a given list of persons and concats their ID.
+        /// </summary>
+        /// <param name="people">List of persons</param>
+        /// <returns>A concatenatd list person id's with a '|' seperator</returns>
+        private static string ConvertPeopleListToString(List<PersonModel> people)
+        {
+            string output = "";
+
+            if (people.Count == 0)
+            {
+                return "";
+            }
+
+            foreach (PersonModel p in people)
+            {
+                output += $"{ p.Id }|";
+            }
+
+            output = output.Substring(0, output.Length - 1);
+
+            return output;
         }
     }
 }
