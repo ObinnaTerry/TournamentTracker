611887558739846efa21867722d4c24dc61ceab0
youngjay90 <3254048915@qq.com>
2020-08-06 15:01:08 +0200

    refactor: Add documentation

--- TrackerLibrary/Models/PersonModel.cs
+++ TrackerLibrary/Models/PersonModel.cs
@@ -1,14 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace TrackerLibrary.Models
 {
     public class PersonModel
     {
+        /// <summary>
+        /// The unique identifier for the person.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// First name for the person
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// last name for the person
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Email address for the person
+        /// </summary>
         public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Cellphone number for the person
+        /// </summary>
         public string CellphoneNumber { get; set; }
+
+        /// <summary>
+        /// Concats First and LAst name
+        /// </summary>
+        public string FullName
+        {
+            get
+            {
+                return $"{ FirstName } { LastName }";
+            }
+        }
     }
 }
