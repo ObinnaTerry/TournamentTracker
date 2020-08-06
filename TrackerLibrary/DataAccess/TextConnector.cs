3d5d30cc7c0f1f4440f733fb07fd6c4245ec798b
youngjay90 <3254048915@qq.com>
2020-08-06 15:03:37 +0200

    feat: Add classes to handle saving person and team models to file storage.

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -9,7 +9,34 @@ namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
-        private const string PrizesFile = "PrizeModels.csv"; //File name for text file storage.
+        private const string PrizesFile = "PrizeModels.csv"; //File name for prize text file storage.
+        private const string PeopleFile = "PersonModels.csv";
+        private const string TeamFile = "TeamModels.csv";
+
+        /// <summary>
+        /// Takes person model and saves it to text file storage.
+        /// </summary>
+        /// <param name="model">Person model</param>
+        /// <returns>person model</returns>
+        public PersonModel CreatePerson(PersonModel model)
+        {
+            List<PersonModel> people = PeopleFile.FullFilePath().LoadFile().ConvertToPersonModels();
+
+            int currentId = 1;
+
+            if (people.Count > 0)
+            {
+                currentId = people.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            people.Add(model);
+
+            people.SaveToPeopleFile(PeopleFile);
+
+            return model;
+        }
 
         /// <summary>
         /// Saves a new prize to a textfile.
@@ -19,8 +46,6 @@ namespace TrackerLibrary.DataAccess
         public PrizeModel CreatePrize(PrizeModel model)
         {
             List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
-
-
 
             int currentId = 1;
 
@@ -35,6 +60,40 @@ namespace TrackerLibrary.DataAccess
             prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
+        }
+
+        /// <summary>
+        /// Takes a team model, formats it and saves it to a target file. 
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>model</returns>
+        public TeamModel CreateTeam(TeamModel model)
+        {
+            List <TeamModel> teams = TeamFile.FullFilePath().LoadFile().ConvertToTeamModels(PeopleFile);
+
+            int currentId = 1;
+
+            if (teams.Count > 0)
+            {
+                currentId = teams.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            teams.Add(model);
+
+            teams.SaveToTeamFile(TeamFile);
+
+            return model;
+        }
+
+        /// <summary>
+        /// Loads a target file contain persons info, converts the info to equivalent person models.
+        /// </summary>
+        /// <returns>A list of person models.</returns>
+        public List<PersonModel> GetPerson_All()
+        {
+            return PeopleFile.FullFilePath().LoadFile().ConvertToPersonModels();
         }
     }
 }
